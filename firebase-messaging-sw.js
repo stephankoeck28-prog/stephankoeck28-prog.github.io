7c1f31425db3752c94a1654586a3b82a17516f7e
stephankoeck28-prog <stephankoeck28@gmail.com>
2026-03-14 15:03:10 +0100

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,34 +1,25 @@
-let lastPush = null;
+importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging-compat.js');
+
+firebase.initializeApp({
+  apiKey: "AIzaSyAemNwKerXt-hvtikIIACR4oBTb72VjL_U",
+  authDomain: "usv-staw-app.firebaseapp.com",
+  projectId: "usv-staw-app",
+  storageBucket: "usv-staw-app.appspot.com",
+  messagingSenderId: "983106867178",
+  appId: "1:983106867178:web:90234121a5833e7e396c93"
+});
+
+const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage((payload) => {
-
   console.log("Push erhalten:", payload);
 
-  const title =
-    payload.notification?.title ||
-    payload.data?.title ||
-    "USV StAW";
-
-  const body =
-    payload.notification?.body ||
-    payload.data?.body ||
-    "Neue Nachricht";
-
-  const msgId = payload.messageId || body;
-
-  // doppelte verhindern
-  if (msgId === lastPush) {
-    console.log("⛔ Doppelter Push blockiert");
-    return;
-  }
-
-  lastPush = msgId;
-
-  self.registration.showNotification(title, {
-    body: body,
-    icon: "/icon-192.png",
-    badge: "/icon-192.png",
-    tag: msgId
-  });
-
+  self.registration.showNotification(
+    payload.notification?.title || "USV StAW",
+    {
+      body: payload.notification?.body || "Neue Nachricht",
+      icon: "/icon-192.png"
+    }
+  );
 });
